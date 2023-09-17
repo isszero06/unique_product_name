63fede201053bc29c69d9518612d5693c6287daf
isszero06 <76986925+isszero06@users.noreply.github.com>
2023-09-17 20:25:52 +0300

    Update module.py

--- unique_product_name/models/module.py
+++ unique_product_name/models/module.py
@@ -1,15 +1,15 @@
 
 
 from odoo import api, Command, fields, models, _
-from odoo.exceptions import ValidationError, UserError
+from odoo.exceptions import ValidationError, UserError,RedirectWarning
 
 class Company(models.Model):
     _inherit = 'res.company'
 
-    sale_unique_product = fields.Boolean(String="Activate Unique Sale order products")
-    purchase_unique_product = fields.Boolean(String="Activate Unique purchase order products")
-    invoice_unique_product = fields.Boolean(String="Activate Unique Invoice products")
-    bill_unique_product = fields.Boolean(String="Activate Unique Bill products")
+    sale_unique_product = fields.Boolean(string="Activate Unique Sale order products")
+    purchase_unique_product = fields.Boolean(string="Activate Unique purchase order products")
+    invoice_unique_product = fields.Boolean(string="Activate Unique Invoice products")
+    bill_unique_product = fields.Boolean(string="Activate Unique Bill products")
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
@@ -22,19 +22,18 @@ class SaleOrder(models.Model):
         else:
             self.sale_unique_product = False
 
-    sale_unique_product = fields.Boolean(default='_compute_sale_unique_product',store=True,String="Activate Unique Sale order products",readonly=False)
+    sale_unique_product = fields.Boolean(default='_compute_sale_unique_product',store=True,string="Activate Unique Sale order products",readonly=False)
 
-class SaleOrderLine(models.Model):
-    _inherit = 'sale.order.line'
-
-
-    @api.constrains('product_id')
+    @api.constrains('order_line.product_id','sale_unique_product')
     def _check_product_id_unique(self):
         for move in self:
-            if move.order_id.sale_unique_product:
-                results = move.search_count([('product_id', '=', move.product_id.id), ('order_id', '=', move.order_id.id)])
-                if results > 1:
-                    raise ValidationError(_('product number already exists!. (Product Name: %s)', move.name))
+            user_so = self.env['res.users'].search([('id', '=', self.env.user.id),('groups_id', 'in', self.env.ref('unique_product_name.group_sales_uniqe_product').id)])
+            if not user_so:
+                if move.sale_unique_product and  len(move.order_line) > 0:
+                    for line in move.order_line.filtered(lambda x: x.display_type not in ('line_section', 'line_note')):
+                        results1 = line.search_count([('product_id', '=', line.product_id.id), ('order_id', '=', line.order_id.id)])
+                        if results1 > 1:
+                            raise ValidationError(_('product number already exists!. (Product Name: %s)', line.product_id.name))
 
 
 
@@ -49,19 +48,18 @@ class PurchaseOrder(models.Model):
         else:
             self.purchase_unique_product = False
 
-    purchase_unique_product = fields.Boolean(default='_compute_purchase_unique_product',store=True,String="Activate Unique Purchase order products",readonly=False)
+    purchase_unique_product = fields.Boolean(default='_compute_purchase_unique_product',store=True,string="Activate Unique Purchase order products",readonly=False)
 
-class PurchaseOrderLine(models.Model):
-    _inherit = 'purchase.order.line'
-
-    @api.constrains('product_id')
+    @api.constrains('order_line.product_id','purchase_unique_product')
     def _check_product_id_unique(self):
         for move in self:
-            if move.order_id.purchase_unique_product:
-                results = move.search_count([('product_id', '=', move.product_id.id), ('order_id', '=', move.order_id.id)])
-                if results > 1:
-                    raise ValidationError(_('product number already exists!. (Product Name: %s)', move.name))
-
+            user_po = self.env['res.users'].search([('id', '=', self.env.user.id),('groups_id', 'in', self.env.ref('unique_product_name.group_purchase_uniqe_product').id)])
+            if not user_po:
+                if move.purchase_unique_product and  len(move.order_line) > 0:
+                    for line in move.order_line.filtered(lambda x: x.display_type not in ('line_section', 'line_note')):
+                        results1 = line.search_count([('product_id', '=', line.product_id.id), ('order_id', '=', line.order_id.id)])
+                        if results1 > 1:
+                            raise ValidationError(_('product number already exists!. (Product Name: %s)', line.product_id.name))
 
 class AccountMove(models.Model):
     _inherit = 'account.move'
@@ -77,22 +75,25 @@ class AccountMove(models.Model):
         return self.env.company.invoice_unique_product
 
 
-    invoice_unique_product = fields.Boolean(default=compute_invoice_unique_product,store=True,String="Activate Unique Invoice products",readonly=False)
+
+    invoice_unique_product = fields.Boolean(default=compute_invoice_unique_product,store=True,string="Activate Unique Invoice products",readonly=False)
 
 
-    bill_unique_product = fields.Boolean(default=compute_bill_unique_product,store=True,String="Activate Unique Bill products",readonly=False)
+    bill_unique_product = fields.Boolean(default=compute_bill_unique_product,store=True,string="Activate Unique Bill products",readonly=False)
 
 
-    @api.constrains('state','invoice_line_ids.product_id','invoice_unique_product','bill_unique_product','move_type')
+    @api.constrains('invoice_line_ids.product_id','invoice_unique_product','bill_unique_product','move_type')
     def _check_product_id_unique(self):
         for move in self:
-            if move.is_sale_document(include_receipts=True) and move.state =='draft':
+            user_inv = self.env['res.users'].search([('id', '=', self.env.user.id),('groups_id', 'in', self.env.ref('unique_product_name.group_invoice_uniqe_product').id)])
+            user_bill = self.env['res.users'].search([('id', '=', self.env.user.id),('groups_id', 'in', self.env.ref('unique_product_name.group_bill_uniqe_product').id)])
+            if not user_inv and move.move_type in ('out_invoice','out_refund'):
                 if move.invoice_unique_product and  len(move.invoice_line_ids) > 0:
                     for line in move.invoice_line_ids.filtered(lambda x: x.display_type not in ('line_section', 'line_note')):
                         results1 = line.search_count([('product_id', '=', line.product_id.id), ('move_id', '=', line.move_id.id)])
                         if results1 > 1:
                             raise ValidationError(_('product number already exists!. (Product Name: %s)', line.product_id.name))
-            if move.is_purchase_document(include_receipts=True) and move.state =='draft':
+            if not user_bill and move.move_type in ('in_invoice','in_refund'):
                 if move.bill_unique_product and  len(move.invoice_line_ids) > 0:
                     for line in move.invoice_line_ids.filtered(lambda x: x.display_type not in ('line_section', 'line_note')):
                         results2 = line.search_count([('product_id', '=', line.product_id.id), ('move_id', '=', line.move_id.id)])
