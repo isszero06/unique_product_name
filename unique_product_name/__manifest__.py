34f2e782977fa68a368ac44659bc79908ae440ec
Zero Systems <isszero06@gmail.com>
2024-01-13 14:57:02 +0200

    Update __manifest__.py

--- unique_product_name/__manifest__.py
+++ unique_product_name/__manifest__.py
@@ -11,7 +11,7 @@
 
 {
     'name': 'Unique Product Name',
-    'version': '6.0.0',
+    'version': '7.0.0',
     'category': 'Sales',
     "author": 'Zero Systems',
     "company": 'Zero for Information Systems',
